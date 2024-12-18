a087c0629fc1b8ab1be60647479d75b35d5e0cd7
Fred Porciúncula <hey@fredporciuncula.com>
2024-12-18 22:47:03 +0100

    Add inset example

--- app/src/main/java/com/fredporciuncula/weirdinsets/MainActivity.kt
+++ app/src/main/java/com/fredporciuncula/weirdinsets/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fredporciuncula.weirdinsets.ui.theme.WeirdInsetsTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             WeirdInsetsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    MainScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(modifier: Modifier = Modifier) {
+    Column {
+        TextField(
+            value = "",
+            onValueChange = {},
+            modifier = modifier.padding(40.dp)
+        )
+        LazyColumn(
+            modifier = Modifier.imePadding(),
+            contentPadding = WindowInsets.navigationBars.asPaddingValues(),
+        ) {
+            repeat(50) {
+                item {
+                    Text(
+                        text = "Mike help $it",
+                        modifier = Modifier.fillMaxWidth(),
+                        style = typography.titleMedium,
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     WeirdInsetsTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
